e196ef6bd59c22ba25a41afa81d99fab3abba510
LordLy15 <adnanpb15@gmail.com>
2026-08-07 01:04:54 +0700

    chore: remove test scripts

--- api/test_api.php
+++
@@ -1,4 +0,0 @@
-<?php
-$_GET['action'] = 'get_global_friends';
-$_GET['user_id'] = 'dnaan';
-require 'api.php';
--- test_trips.php
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<?php
-$pdo = new PDO('sqlite:c:\xampp\htdocs\tripnan\api\tripnan.sqlite');
-$stmt = $pdo->query('SELECT * FROM trips');
-$trips = $stmt->fetchAll(PDO::FETCH_ASSOC);
-print_r($trips);
-?>
